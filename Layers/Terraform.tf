d096f9169c98c34cf3010e103e93f7385bf6b19b
prathyusha-19480 <81969652+prathyusha-19480@users.noreply.github.com>
2021-06-03 17:17:00 +0530

    Update Terraform.tf

--- Layers/Terraform.tf
+++ Layers/Terraform.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source = "../Modules/ec2"
   region = "us-east-1"
-  availability_zones = "us-west-2a"
+  availability_zones = "us-east-1a"
   ami = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
 
